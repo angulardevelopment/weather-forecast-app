050af8d55ef428d6844f17317f27005cf5dbf93b
DESKTOP-1ASQ7K6\sunny <sunny.gupta@docquity.com>
2020-12-13 17:51:42 +0530

    averaging

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -78,7 +78,7 @@ const routes: Routes = [
 
 @NgModule({
     imports: [
-        RouterModule.forRoot(routes, { useHash: true }),
+        RouterModule.forRoot(routes),
         CommonModule
     ],
     exports: [
--- src/app/home/stock/stock.component.ts
+++ src/app/home/stock/stock.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stock.component.css']
 })
 export class StockComponent implements OnInit {
-
+  firstBuyPrice;
+  firstShare;
+  totalFirstPrice;
+  secondBuyPrice;
+  secondShare;
+  totalSecondPrice;
+  averagePrice;
   constructor() { }
 
   ngOnInit() {
   }
 
+  getFirstTotal (){
+    this.totalFirstPrice = this.firstBuyPrice * this.firstShare;
+  }
+
+  getSecondTotal (){
+    this.totalSecondPrice = this.secondBuyPrice * this.secondShare;
+    this.averagePrice = (this.totalFirstPrice + this.totalSecondPrice)/(this.firstShare + this.secondShare);
+  }
 }
